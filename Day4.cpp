d1ca566c715fe538e533a86b367b975643e1673b
Sawera <saweraazam08@gmail.com>
2026-05-25 19:50:10 +0500

    Day 4: Pointer basis, derefrencing ,heap vs stack, Dangling Pointer,Null, memory leak

--- Day4.cpp
+++ Day4.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int num = 5;
+	int *iptr;
+	iptr = &num;
+	
+	cout << "num " << num << endl;
+	cout << "&num " << &num << endl;
+	cout << "iptr " << iptr << endl;
+	cout << "*iptr " << *iptr << endl;  // de-referencing
+
+	*iptr = 1009;//assigning value indirectly through derefrencing
+	cout << "num " << num << endl;
+	//Stack vs heap
+    //Dangling pointer, memory leakage, Null pointer
     int x=5;//variable on stack
     int*p=new int(10);//variable on heap
     cout<<"x= "<<x<<endl;
